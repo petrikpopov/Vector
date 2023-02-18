601e5246c728d16df6a18e869fcb91f977b2ed4a
petrikpopov <112963523+petrikpopov@users.noreply.github.com>
2023-02-18 17:13:13 +0200

    Add files via upload

---
+++ EmptyStruct.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Struct16._02._2023HM
+{
+    public struct Vector
+    {
+        public int a;
+        public int b;
+        public int c;
+
+       
+        public Vector(int A, int B, int C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public void Umn(int r)
+        {
+            Console.WriteLine("Результат умножения вектора на число");
+            a *= r;
+            b *= r;
+            c *= r;
+        }
+        public void Summ(int r)
+        {
+            Console.WriteLine("Результат сумирования вектора и числа");
+            a += r;
+            b += r;
+            c += r;
+        }
+        public void VectoMinus(Vector A)
+        {
+            Console.WriteLine("Результат разницы двух векторов");
+            a -= A.a;
+            b -= A.b;
+            c -= A.c;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"A = {a}\tB = {b}\tC = {c}\t");
+        }
+        public override string ToString()
+        {
+            return $"A{a}\tB{b}\tC{c}\t";
+        }
+
+    }
+   
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Struct16._02._2023HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Vector obj = new Vector(4,5,6);
+            obj.Umn(3);
+            obj.Print();
+            Console.WriteLine("\n");
+            Vector obj1 = new Vector(4, 5, 6);
+            obj1.Summ(3);
+            obj1.Print();
+            Console.WriteLine("\n");
+            Vector obj0 = new Vector(3, 2, 3);
+            Vector obj2 = new Vector(7, 6, 9);
+            obj2.VectoMinus(obj0);
+            obj2.Print();
+        }
+
+        
+    }
+}
